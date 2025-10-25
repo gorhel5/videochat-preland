50ed1f38c45ab1bec57f916fe7018b9e73b53c6e
gorhel5 <136500168+gorhel5@users.noreply.github.com>
2025-10-26 00:02:41 +0200

    Update Facebook Pixel ID for tracking

--- components/FacebookPixel/FacebookPixel.tsx
+++ components/FacebookPixel/FacebookPixel.tsx
@@ -16,7 +16,7 @@ export default function FacebookPixel() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1554794815953931');
+            fbq('init', '1565781467923350');
             fbq('track', 'PageView');
           `,
         }}
